cd94984a950487ad26c7752e66898458d38a2126
Tiko <tikhon102030@gmail.com>
2026-06-19 13:13:46 -0700

    Server now broadcasts recieved messages to all clients. Reworked both send/recieve packet methods to be more consistent across server and client

--- src/client.rs
+++ src/client.rs
@@ -1,6 +1,8 @@
-use std::io::Write;
+use std::io::{Read, Write};
 use std::io;
+use std::fs::File;
 use std::net::TcpStream;
+use std::thread;
 extern crate serde;
 use serde::{Serialize, Deserialize};
 
@@ -24,23 +26,44 @@ enum Packet {
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Settings {
-    server_address: String,
+    server_ip: String,
     server_port: String,
 }
 
 fn main() {
-    let mut stream = TcpStream::connect("0.0.0.0:2345").unwrap();
-    println!("Connected to server!");
-
     println!("Enter name: ");
     let name = get_input().trim().to_string();
+
+    let server_ip;
+    let server_port;
+    loop {
+        match get_settings() {
+            Some(settings) => {
+                println!("Got settings: ip={}, port={}", settings.server_ip, settings.server_port);
+                server_ip = settings.server_ip;
+                server_port = settings.server_port;
+                break;
+            }
+            None => {
+                set_settings();
+            }
+        }
+    }
+
+    let address = format!("{}:{}", server_ip, server_port);
+    let mut stream = TcpStream::connect(address).unwrap();
+
+    let mut read_stream = stream.try_clone().unwrap();
+    thread::spawn(move || {
+        listen_for_packets(&mut read_stream);
+    });
 
     let packet = Packet::Join {
         name: name.clone(),
     };
+    println!("Connected to server!");
 
     let json = serde_json::to_string(&packet).unwrap();
-    // println!("\nSending |{}|", json);
     let _ = stream.write_all(json.as_bytes()).unwrap();
 
     loop {
@@ -57,11 +80,51 @@ fn main() {
             message: message,
         };
 
-        let json = serde_json::to_string(&packet).unwrap();
-        // println!("\nSending |{}|", json);
-
-        let _ = stream.write_all(json.as_bytes()).unwrap();
+        send_packet(packet, &mut stream);
     }
+}
+
+fn listen_for_packets(mut stream: &mut TcpStream) {
+    loop {
+        let Some(data) = recieve_packet(&mut stream)
+            else {
+                println!("Lost connection with server");
+                break;
+            };
+
+        match data {
+            Packet::Chat {name, message} => {
+                println!("{}: {}", name, message);
+            }
+            Packet::Join {name} => {
+                println!("----- {} joined the chat -----", name);
+            }
+            Packet::Leave {name} => {
+                println!("----- {} left the chat -----", name);
+            }
+            _ => {
+                println!("Unexpected packet");
+            }
+        }
+    }
+}
+
+fn send_packet(packet: Packet, stream: &mut TcpStream) {
+    let json = serde_json::to_string(&packet).unwrap();
+
+    let _ = stream.write_all(json.as_bytes()).unwrap();
+}
+
+fn recieve_packet(stream: &mut TcpStream) -> Option<Packet> {
+    let mut buffer = [0; 1024];
+    let bytes_read = stream.read(&mut buffer).unwrap();
+
+    if bytes_read == 0 {
+        return None;
+    }
+
+    let text = String::from(String::from_utf8_lossy(&buffer[..bytes_read]));
+    serde_json::from_str(&text).unwrap()
 }
 
 fn get_input() -> String {
@@ -70,4 +133,70 @@ fn get_input() -> String {
     let _ = io::stdin().read_line(&mut input);
 
     input
+}
+
+fn get_settings() -> Option<Settings> {
+    let mut file = match File::open("settings.json") {
+        Ok(file) => file,
+        Err(_) => {
+            return None // if file doesn't exist
+        }
+    };
+
+    let mut data = String::new();
+
+    match file.read_to_string(&mut data) {
+        Ok(_) => {}
+        Err(_) => {
+            return None // if file corrupted
+        }
+    }
+
+    let parsed: Result<Settings, _> = serde_json::from_str(&data);
+    match parsed {
+        Ok(settings) => {
+            Some(settings)
+        }
+        Err(e) => {
+            println!("Error: {}", e);
+
+            None
+        }
+    }
+}
+
+fn set_settings() {
+    print!("Would you like to set settings or use defaults? (Default/yes): ");
+    io::stdout().flush().unwrap();
+    let decision = get_input().trim().to_string().to_lowercase();
+
+    let server_ip: String;
+    let server_port: String;
+
+    if decision == "default" || decision == "d" {
+        server_ip = String::from("0.0.0.0");
+        server_port = String::from("2345");
+    }
+    else if decision == "yes" || decision == "y" {
+        print!("Enter the server IP: ");
+        io::stdout().flush().unwrap();
+        server_ip = get_input().trim().to_string();
+
+        print!("Enter the server port: ");
+        io::stdout().flush().unwrap();
+        server_port = get_input().trim().to_string();
+    }
+    else {
+        println!("Must enter 'yes' or 'default'");
+        return;
+    }
+
+    let settings: Settings = Settings {
+        server_ip,
+        server_port,
+    };
+
+    let json_data = serde_json::to_string(&settings).unwrap();
+    let mut file = File::create("settings.json").unwrap();
+    let _ = file.write_all(json_data.as_bytes());
 }
--- src/server.rs
+++ src/server.rs
@@ -1,6 +1,7 @@
-use std::io::Read;
+use std::io::{Read, Write};
 use std::thread;
 use std::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex};
 extern crate serde;
 use serde::{Serialize, Deserialize};
 
@@ -23,25 +24,32 @@ enum Packet {
 }
 
 fn main() {
-    let stream = TcpListener::bind("0.0.0.0:2345").unwrap();
+    let connection = TcpListener::bind("0.0.0.0:2345").unwrap();
     println!("Server started");
 
-    for connection in stream.incoming() {
+    let clients = Arc::new(Mutex::new(Vec::<TcpStream>::new()));
+
+    for stream in connection.incoming() {
+        let stream = stream.unwrap();
+        clients.lock().unwrap().push(stream.try_clone().unwrap());
+
+        let clients_clone = Arc::clone(&clients);
+
         thread::spawn(move || {
-            handle_client(connection.unwrap());
+            handle_client(stream, clients_clone);
         });
     }
 }
 
-fn handle_client(mut stream: TcpStream) {
+fn handle_client(mut stream: TcpStream, clients: Arc<Mutex<Vec<TcpStream>>>) {
     loop {
-        let Some(data) = recieve_data(&mut stream)
-        else {
-            println!("Client disconnected");
-            break;
-        };
+        let Some(data) = recieve_packet(&mut stream)
+            else {
+                println!("Client disconnected");
+                break;
+            };
 
-        match data {
+        match &data {
             Packet::Chat {name, message} => {
                 println!("{}: {}", name, message);
             }
@@ -55,10 +63,16 @@ fn handle_client(mut stream: TcpStream) {
                 println!("Unexpected packet");
             }
         }
+
+        let mut clients = clients.lock().unwrap();
+
+        for client in clients.iter_mut() {
+            send_packet(&data, client);
+        }
     }
 }
 
-fn recieve_data(stream: &mut TcpStream) -> Option<Packet> {
+fn recieve_packet(stream: &mut TcpStream) -> Option<Packet> {
     let mut buffer = [0; 1024];
     let bytes_read = stream.read(&mut buffer).unwrap();
 
@@ -68,4 +82,10 @@ fn recieve_data(stream: &mut TcpStream) -> Option<Packet> {
 
     let text = String::from(String::from_utf8_lossy(&buffer[..bytes_read]));
     serde_json::from_str(&text).unwrap()
+}
+
+fn send_packet(packet: &Packet, stream: &mut TcpStream) {
+    let json = serde_json::to_string(&packet).unwrap();
+
+    let _ = stream.write_all(json.as_bytes()).unwrap();
 }
